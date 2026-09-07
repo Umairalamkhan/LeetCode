d53f576c5b06b36f989690191887dd9175818955
Umair Alam Khan <120217803+Umairalamkhan@users.noreply.github.com>
2026-09-07 21:08:24 +0500

    Time: 0 ms (100.00%) | Memory: 8.5 MB (93.34%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -2,7 +2,10 @@ class Solution {
 public:
     double myPow(double x, int n) {
         if(n==0)return 1.0;
-        
+        if(x==0)return 0.0;
+        if(x==1)return 1.0;
+        if(x==-1 && n%2==0)return 1.0;
+        if(x==-1 && n%2!=0)return -1.0;
         long long binForm=n;
         if(binForm<0){
             x=1/x;
